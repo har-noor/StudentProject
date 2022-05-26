d53c022150f25615e424aa2b47a54e114ad900e1
har-noor <kau11900@sheridancollege.ca>
2022-05-26 10:33:45 -0400

    new repo for student code...

--- src/printingstudentdetails/Printingstudentdetails.java
+++ src/printingstudentdetails/Printingstudentdetails.java
@@ -41,8 +41,10 @@ public class Printingstudentdetails {
             System.out.println("Learn github operations");
         }
         
-    //commit and save
-    //pull operation
+        //commit and save
+        //pull operation
+        
+   
     
     
         
